79193f72a48c6f0e13533ffebea25ecd0570d82b
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-02 14:29:47 +0545

    Connect backend with frontend

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!');
+})
+
+app.listen(5000, () => {
+	console.log("App working in port 5000");
+});

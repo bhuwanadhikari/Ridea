dbf5076ab909c685323684bbd11788aabca65c16
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-12 19:46:08 +0545

    Connect MongoDB

---
+++ config/keys_development.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://bhuwan1:bhuwan1@cluster0-t6cac.mongodb.net/test?retryWrites=true&w=majority',
+  secret: 'rideasecret'
+};
--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
 
 const app = express();
 
+//database
+const db = require('./config/keys').mongoURI;
+
+
+//connection to database
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to the mongoose"))
+    .catch(err => console.log(err));
+
+//Middleware for passport
+app.use(passport.initialize());
+
+//Configuration for passport
+require('./config/passport')(passport);
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
-})
+});
 
 app.listen(5000, () => {
 	console.log("App working in port 5000");

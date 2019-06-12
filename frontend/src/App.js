e8ec2fcd93f062a193f803b9cd47ee11badd0b46
jiwansapkota <jwnspkt@gmail.com>
2019-06-12 22:10:37 +0545

    Background map added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,11 @@ class App extends Component{
       <NavBar clicked={this.drawerToggleClickHandler}/>
       {sidedrawer}
       {backdrop}
-      <div className='bodyWrapper'></div>
+      <div className='bodyWrapper'>
+      <iframe title='Maps in Ridea app' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56239.647840202364!2d83.99021427673622!3d28.23834772153084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcd98c6b6d110d582!2sPaschimanchal+Campus+(I.O.E)!5e0!3m2!1sen!2snp!4v1560354940027!5m2!1sen!2snp"     
+        allowfullscreen style={{border:0 ,height:'100vh', width:'100vw'}}>
+        </iframe>
+           </div>
       </div>
   );
 }

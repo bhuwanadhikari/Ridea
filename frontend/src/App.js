57c841870ac972491b616ce0475235f12b7fff4c
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-13 23:18:56 +0545

    Edit iframe tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,7 +47,7 @@ class App extends Component{
       {backdrop}
       <div className='BodyWrapper'>
         <iframe title='Maps in Ridea app' className='MapFrame' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56239.647840202364!2d83.99021427673622!3d28.23834772153084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcd98c6b6d110d582!2sPaschimanchal+Campus+(I.O.E)!5e0!3m2!1sen!2snp!4v1560354940027!5m2!1sen!2snp"     
-        allowfullscreen style={{border:'0',height:'100vh', width:'100vw', top:'0', left:'0',position:"absolute",'z-index':'-100'}}>
+         style={{border:'0',height:'100vh', width:'100vw', top:'0', left:'0',position:"absolute",'z-index':'-100'}}>
         </iframe>
       </div>
      <Footer/>

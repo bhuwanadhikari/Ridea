3c1cf1c4dbfbcb7220748a100c7c0ebaa88b2ffa
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-14 00:04:06 +0545

    Manage App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,33 @@
-import React,{Component} from 'react';
-import NavBar from './components/NavBar/NavBar';
-import SideDrawer from './components/SideDrawer/SideDrawer';
-import Backdrop from './components/Backdrop/Backdrop';
-import Footer from './components/Footer/Footer'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const styles={
-  // transition: 'all 3s ease-in'
-  'transition': 'width 2s ease-in',
-  'transition-delay': '2s',
-  'transitionn-timeout':'2s'
-  // '--webkit-transition': 'width 2s'
-}
-class App extends Component{
+import Home from './containers/Home';
+import Landing from './containers/Landing';
+
+
+
+class App extends Component {
   constructor(props) {
     super(props)
-  
-    this.state={
-      sideDrawerOpens:false,
-      width: '0%'
+
+    this.state = {
+
     }
   }
-  
- drawerToggleClickHandler =()=>{
-   this.setState({width:'70%'});
-    this.setState((prevState)=>({ sideDrawerOpens:!prevState.sideDrawerOpens}))
 
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <div style={{ height: '100%' }} className="App">
+            <Route exact path = "/home" component = {Home} />
+            <Route exact path = "/" component = {Landing} />
+          </div>
+        </Switch>
+      </Router>
+    );
   }
-  onBackdropClickHandler=()=>{
-    this.setState({sideDrawerOpens:false})
-}
-
-// onShowHandler = () => {
-//   this.setState({myState:'block'});
-// }
-
-//this is test
-  render(){
-    let sidedrawer;
-    let backdrop;
-    if(this.state.sideDrawerOpens){
-      sidedrawer=<SideDrawer Style={{...styles,width:this.state.width}}/>;
-      backdrop=  <Backdrop clicked={this.onBackdropClickHandler}/>;
-    }
-  return (
-    <div  style={{height:'100%'}}className="App">
-      <NavBar clicked={this.drawerToggleClickHandler}/>
-      {sidedrawer}
-      {backdrop}
-      <div className='BodyWrapper'>
-        <iframe title='Maps in Ridea app' className='MapFrame' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56239.647840202364!2d83.99021427673622!3d28.23834772153084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcd98c6b6d110d582!2sPaschimanchal+Campus+(I.O.E)!5e0!3m2!1sen!2snp!4v1560354940027!5m2!1sen!2snp"     
-         style={{border:'0',height:'100vh', width:'100vw', top:'0', left:'0',position:"absolute",'z-index':'-100'}}>
-        </iframe>
-      </div>
-     <Footer/>
-    </div>
-  );
-}
 }
 
 

b31743426f6a09f0ac59f476eb82678a8a1b5e84
jiwansapkota <jwnspkt@gmail.com>
2019-06-19 00:04:43 +0545

    back on track

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import './Chat.css';
-// import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import ChatToolBar from './ChatToolBar';
 import ChatThumbnail from './ChatThumbnail';
+
 class Chat extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            routeBack:'/home'        }
+    }
     render() {
         return (
            
             <div className="ChatPageContainer">
-             <div className="ChatToolbarContainer"> <ChatToolBar/></div>  
-             <div className="ChatThumbnailContainer"></div>  
+             <div className="ChatToolbarContainer"> <ChatToolBar route={this.state.routeBack}/></div>  
+             <div className="ChatThumbnailsWrapper">
+             <ChatThumbnail/><ChatThumbnail/><ChatThumbnail/><ChatThumbnail/>
+             <ChatThumbnail/><ChatThumbnail/><ChatThumbnail/><ChatThumbnail/><ChatThumbnail/>
+             <ChatThumbnail/><ChatThumbnail/><ChatThumbnail/><ChatThumbnail/><ChatThumbnail/>
+             </div> 
+             <div className="MessagesWrapper"> here is your message</div> 
             </div>
         )
     }

2f455b50ccfcd47a36c75de7597e51405cb1f9c7
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-22 22:56:00 +0545

    Set environment variables for deployment

--- config/keys_production.js
+++ config/keys_production.js
@@ -1,5 +1,7 @@
 
 module.exports = {
    mongoURI: process.env.mongoURI,
-   secret: process.env.secret
+   secret: process.env.secret,
+   clientID: process.env.clientID,
+   clientSecret: process.env.clientSecret
 };

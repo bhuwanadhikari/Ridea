0d493c22c435fbbfae738ee026273b34e4e2de6b
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-22 23:41:01 +0545

    Update the frontend paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home/Home';
 import Landing from './containers/Landing/Landing';

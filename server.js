ed6212523dfe83fde5dd85e4a5c765065469cceb
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-06-22 22:32:18 +0545

    Prepare to deploy

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ if(process.env.NODE_ENV === 'production'){
     });
  }
 
-app.listen(5000, () => {
-    console.log("App working in port 5000");
-});
+
+ const port = process.env.PORT || 5000;
+ app.listen(port, () => console.log(`Server running in port ${port}`));

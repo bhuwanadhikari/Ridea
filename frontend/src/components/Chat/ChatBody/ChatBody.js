962c12bb3801440026985b263bfd9f1e0446fa91
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-07-27 23:53:44 +0545

    Make partner's location displayed

--- frontend/src/components/Chat/ChatBody/ChatBody.js
+++ frontend/src/components/Chat/ChatBody/ChatBody.js
@@ -11,7 +11,7 @@ import './ChatBody.css';
 import io from 'socket.io-client';
 import store from '../../../redux/store/store';
 
-const socketUrl = 'http://localhost:3231';
+const socketUrl = 'https://ridea-chat.herokuapp.com/';
 
 
 class ChatBody extends Component {

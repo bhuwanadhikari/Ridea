8367492b929236f6301ccff8146650d5701ea678
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-08-03 20:39:11 +0545

    Check public vapid key

--- frontend/src/config/keys.js
+++ frontend/src/config/keys.js
@@ -1,8 +1,9 @@
 console.log("Evvironment of the project is:", process.env.NODE_ENV);
 console.log("Evvironment of the project is:", process.env);
+console.log("Public vapid key is:", process.env.PUBLIC_VAPID_KEY);
 
 if (process.env.NODE_ENV === "production") {
    module.exports = require('./keys_production');
 } else {
-   module.exports = require('./keys_production');
+   module.exports = require('./keys_development');
 }

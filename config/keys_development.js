e3a66ec8ae06377bbf1d76a8894a28540d8aef22
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-08-05 18:51:29 +0545

    Update keys

---
+++ config/keys_development.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  MONGO_URI: 'your mongo uri',
+  SECRET: 'your secret',
+  CLIENT_ID: 'client id given by google',
+  CLIENT_SECRET: 'client secret given by google',
+  PUBLIC_VAPID_KEY: 'vapid key for push notification', // not required
+  PRIVATE_VAPID_KEY: 'vapid key for push notification', // not required
+};
